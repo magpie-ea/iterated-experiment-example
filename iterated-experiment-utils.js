652e926b88b81d83ccad7db3a3468a1def7158c7
JannisBush <33023300+JannisBush@users.noreply.github.com>
2019-01-05 12:27:15 +0100

    Only call flattenData once
    flattenData(data) is already called in line 62 and will produce an error if you call it again in line 117

--- iterated-experiment-utils.js
+++ iterated-experiment-utils.js
@@ -114,7 +114,7 @@ const iteratedExperimentUtils = {
                         }
                     });
             } else {
-                const flattenedData = flattenData(data);
+                const flattenedData = data;
                 jQuery("<div/>", {
                     class: "babe-debug-results",
                     html: formatDebugData(flattenedData)
