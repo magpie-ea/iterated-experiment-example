54546932cb2a4a9c263e6ad66ecde3127badbf31
Xiang Ji <hi@xiangji.me>
2021-08-23 03:35:47 +0200

    Work with the server deployed on Heroku

--- main.js
+++ main.js
@@ -12,13 +12,13 @@ $('document').ready(function () {
         // views_seq: [init, intro, instructions, lobby, game, thanks],
         views_seq: [init, intro, instructions, lobby, trial, thanks],
         deploy: {
-            experimentID: '2',
-            // experimentID: "19",
-            // serverAppURL:
-            // "https://babe-demo.herokuapp.com/api/submit_experiment/",
-            serverAppURL: 'http://localhost:4000/api/submit_experiment/',
-            // socketURL: "wss://babe-demo.herokuapp.com/socket",
-            socketURL: 'ws://localhost:4000/socket',
+            experimentID: '1',
+            // experimentID: "1",
+            serverAppURL:
+                'https://magpie-refactored-2.herokuapp.com/api/submit_experiment/',
+            // serverAppURL: 'http://localhost:4000/api/submit_experiment/',
+            socketURL: 'wss://magpie-refactored-2.herokuapp.com/socket',
+            // socketURL: 'ws://localhost:4000/socket',
             deployMethod: 'directLink',
             contact_email: 'YOUREMAIL@wherelifeisgreat.you',
             prolificURL:
